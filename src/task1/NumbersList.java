8721b84f214c0b1e888b58cc1a200952dd475b87
RustamAleev2023 <ridermix@yandex.ru>
2024-02-09 14:03:31 +0200

    Task1 done

--- src/task1/NumbersList.java
+++ src/task1/NumbersList.java
@@ -11,25 +11,17 @@ public class NumbersList {
         this.numbers = new ArrayList<>();
     }
 
-    public List<Integer> getNumbers() {
-        return numbers;
-    }
-
     public void add(int element){
         numbers.add(element);
     }
 
-    public boolean delete(int element) {
-        boolean result;
+    public void delete(int element) {
         if (numbers.contains(element)) {
             int index = numbers.indexOf(element);
             numbers.remove(index);
-            result = true;
         } else {
             System.out.println("Такого элемента нет");
-            result = false;
         }
-        return  result;
     }
 
     public void print() {
@@ -37,25 +29,21 @@ public class NumbersList {
         for (int i = 0; i < numbers.size(); i++) {
             joiner.add(String.valueOf(numbers.get(i)));
         }
-        System.out.println(joiner.toString());
+        System.out.println(joiner);
     }
 
-    public boolean isContains(int element){
+    public void isContains(int element){
         String result = numbers.contains(element) ? "Содержит" : "Не содержит";
         System.out.println(result);
-        return numbers.contains(element);
     }
 
-    public boolean update(int oldElement, int newElement){
-        boolean result = false;
+    public void update(int oldElement, int newElement){
         if(numbers.contains(oldElement)){
             int index = numbers.indexOf(oldElement);
             numbers.remove(index);
             numbers.add(index, newElement);
-            result = true;
         } else {
             System.out.println("Такого элемента нет");
         }
-        return result;
     }
 }
